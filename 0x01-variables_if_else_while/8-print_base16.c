42dc66b8aa583aee4f234339923e1c89ee0b9a36
bereket kume <bereketkume@gmail.com>
2023-09-08 22:09:10 +0000

    done

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,18 +1,18 @@
-#include <stdio.h>
+#include <stdio>
 /**
-*main - display 16 base number
-*
-*Return: Always (0)
-*/
+ *main -  display 16 base number
+ *
+ * Return: Always (0)
+ */
 int main(void)
 {
 	int num;
 	char letter;
 
-	for (num = 0; num <10; num++)
+	for (num = 0; num < 10; num++)
 		putchar(num);
 	for (letter = 'a'; letter <= 'f'; letter++)
-		putchar(letter)
+		putchar(letter);
 	putchar('\n');
 	return (0);
 }
