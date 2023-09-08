33ed66a75543bd68f8e28a71116d692b18a18922
bereket kume <bereketkume@gmail.com>
2023-09-08 21:41:36 +0000

    done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - display number with base 10
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	int num;
+	for (num = 0; num < 10; num++)
+		putchar(num);
+	putchar("\n");
+	return (0);
+}
