be9ca303bd1b06ff022f76749eee0f40b9455e55
bereket kume <bereketkume@gmail.com>
2023-09-13 20:56:50 +0000

    done

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_to_98 - print number
+ *@n: parametet of function
+ *Return: nothing
+ */
+void print_to_98(int n)
+{
+	if ( n <= 98)
+	{
+		for ( i = n; i < 99; i++)
+			printf("%d, ",i);
+	}
+	else {
+		for (i = n; i >= 98; i--)
+			printf("%d, ",i);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,3 +12,4 @@ int print_last_digit(int i);
 void jack_bauer(void);
 void times_table(void);
 int add(int num1, int num2);
+void print_to_98(int n);
