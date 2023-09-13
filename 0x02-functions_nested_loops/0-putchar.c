6f1bcc077fadef79fbe516849ce00a4f3651ea0d
bereket kume <bereketkume@gmail.com>
2023-09-13 10:05:59 +0000

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,6 @@
  */
 int main(void)
 {
-   char school[10] = "_putchar";
-
-   	int i;
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(school[i]);
-	}
-	_putchar(10);
-	return (0)
+	putchar("_putchar");
+	return (0);
 }
