16529e0c1e4eff19b4a84353dded3693e6fa30ba
bereket kume <bereketkume@gmail.com>
2023-09-13 18:32:03 +0000

    done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_abs - give us absoulte value of number
+ *@int value passed as parameter
+ *Return: Always 0
+ */
+int _abs(int)
+{
+	if (i > 0)
+		return (i);
+	else if (i < 0)
+		return (-i);
+	else
+		return (0);
+}
