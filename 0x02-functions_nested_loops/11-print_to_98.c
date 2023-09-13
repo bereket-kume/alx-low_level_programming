0528645a2349aa33502c858c345e595f79240fcb
bereket kume <bereketkume@gmail.com>
2023-09-13 21:20:30 +0000

    done

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,12 +12,12 @@ void print_to_98(int n)
 	{
 		for (i = n; i < 98; i++)
 			printf("%d, ", i);
-		printf("%d", 98);
+		printf("%d ", 98);
 	}
 	else
 	{
 		for (i = n; i > 98; i--)
 			printf("%d, ", i);
-		printf("%d", 98);
+		printf("%d ", 98);
 	}
 }
