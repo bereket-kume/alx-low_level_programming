5450e3eef1004b4f1ea8750b2012b93c08cbb3b0
bereket kume <bereketkume@gmail.com>
2023-09-13 20:34:34 +0000

    done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *add - add two number together
+ *
+ *Return: Always 0
+ */
+int add(int num1,num2)
+{
+	int sum = num1 + num2;
+	return (sum);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,3 +11,4 @@ int _abs(int n);
 int print_last_digit(int i);
 void jack_bauer(void);
 void times_table(void);
+int add(int num1, int num2);
