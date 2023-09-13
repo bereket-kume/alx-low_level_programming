905182268c95a6514c7c33045e21216e13e1e72d
bereket kume <bereketkume@gmail.com>
2023-09-13 10:01:22 +0000

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
 #include <main.h>
 /**
- * main - entry point of code
+ * main - entry point to return text
  *
  *Return: Always(0)
  */
 int main(void)
 {
-	putchar("_putchar\n");
-	return 0;
+   char school[10] = "_putchar";
+
+   	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(school[i]);
+	}
+	_putchar(10);
+	return (0)
 }
