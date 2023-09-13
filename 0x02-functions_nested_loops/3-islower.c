b9faa8a49bc116edefde8a0e3d880d11b313b8ae
bereket kume <bereketkume@gmail.com>
2023-09-13 17:52:49 +0000

    done

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,6 +2,7 @@
 /**
  *islower.c - is to check if letter is lower case
  *c is used as prameter
+ *_islower: used to check for alphabet
  *Return: 1 if lower case , 0 if upper case
  */
 int _islower(int c)
