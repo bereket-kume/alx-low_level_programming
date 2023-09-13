194c48bccddee3c9308f14f51c9b1e4fb989948b
bereket kume <bereketkume@gmail.com>
2023-09-13 18:19:59 +0000

    done

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,6 +2,7 @@
 /**
  *print_sign: check wether the number postive
  *or negative 
+ *@n: character to passed as parameter
  *Return: Always 1 or 0 or -1
  */
 int print_sign(int n)
@@ -11,7 +12,7 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n < 0)n
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
