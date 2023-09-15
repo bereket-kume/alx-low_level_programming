b3c693af13faf5a9d3aaa4da815058b8282655b7
bereket kume <bereketkume@gmail.com>
2023-09-15 08:41:39 +0000

    done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_numbers - is function that prints number
+ *
+ *Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int i = 48;
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,15 @@
+#ifndef MAN_H
+#define MAN_H
+
 /**
  *file continer
  */
-int _putchar(char);
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
+int _putchar(char);
+
+
+#endif /* MATN_H */
