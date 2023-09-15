9442984f76e380ef3d5f9d676830cb0e7766d567
bereket kume <bereketkume@gmail.com>
2023-09-15 17:46:07 +0000

    done

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ *main - is the entery point of the code
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+	int index;
+
+	for (index = 1; index <= 100; index++)
+	{
+		if (index % 3 == 0 && index % 5 == 0)
+		{
+			printf("%s ","FizzBuzz");
+		}
+		else if (index % 3 == 0)
+		{
+			printf("%s ","Fizz");
+		}
+		else if (index % 5 == 0)
+		{
+			printf("%s ","Buzz");
+		}
+		else
+		{
+			printf("%d ", index);
+		}
+	}
+	return (0);
+}
