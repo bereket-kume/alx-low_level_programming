60c5e4b3c8473f5a6426e0fb99b48ab39ed44099
bereket kume <bereketkume@gmail.com>
2023-09-15 12:48:02 +0000

    done

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *print_diagonal - is function to print diagonal
+ *@n: is parameter that is given to the function
+ */
+void print_diagonal(int n)
+{
+	int jen,index;
+
+	if (n > 0)
+	{
+		for (jen = 0; jen < n; jen++)
+		{
+			for (index = 0; index < jen; index++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
+
