ba9a84893e90a7bb70b52462e4201a9e862ccc8a
bereket kume <bereketkume@gmail.com>
2023-09-15 08:04:11 +0000

    done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *mul - is function that add two number
+ *@a: is first parameter
+ *@b: is second prarameter
+ *Return: nothing
+ */
+int mul(int a, int b)
+{
+	int sum= a * b;
+	return (sum);
+}
