bf0d48f97e0c6bdf22706350fdf97bd3dcc02ec5
bereket-kume <bereketkume@gmail.com>
2023-09-18 08:39:25 -0700

    done

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  *reset_to_98 - is function that decleared to change number
- *@*n: is parameter of the function
+ *@n: is parameter of the function
  *Return: nothing
  */
 void reset_to_98(int *n)
