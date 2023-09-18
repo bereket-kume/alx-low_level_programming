bc2ea95289998b5e9fd000a40daa5a91d9846090
bereket-kume <bereketkume@gmail.com>
2023-09-18 09:08:56 -0700

    done

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_puts - is our function that display string
+ *@str: is parameter of our function
+ *Return: nothing
+ */
+void _puts(char *str)
+{
+	printf("%s", str);
+}
+
