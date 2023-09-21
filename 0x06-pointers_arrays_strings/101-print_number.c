8c655b57ab9f92c0abf5bed6e61991a6097af78d
bereket-kume <bereketkume@gmail.com>
2023-09-21 14:24:04 -0700

    done

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ */
+void print_number(int n)
+{
+	_putchar(n);
+}
---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,10 @@
+ #include <unistd.h>
+/**
+ *  * _putchar - is function that display
+ *   *@c: is parameter our function
+ *    *Return: 1
+ *     */
+int _putchar(char c)
+{
+		return (write(1, &c, 1));
+}
