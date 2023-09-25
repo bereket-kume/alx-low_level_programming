589802e67d4a6f3065fbb5bd8744fef131897158
bereket-kume <bereketkume@gmail.com>
2023-09-25 04:50:58 -0700

    sone

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _memset - is that fills memory with a constant byte
- * @s: is our first parameter
- * @n: is our second parameter
- * @b: is our third function
- * return: always s
+ *_memset - is the function that fills memory with a constant byte.
+ *@s: is our first parameter
+ *@n: is our second parameter
+ *@b: is our third function
+ *Return: always s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
