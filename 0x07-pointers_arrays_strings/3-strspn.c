2729efd05854db3c3ea047c93b4bbae12254db4e
bereket-kume <bereketkume@gmail.com>
2023-09-25 04:59:50 -0700

    done

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * _strspn - is function  that gets the length of a prefix substring.
  *@s: is first parameret
@@ -7,8 +8,9 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j;
-	unsigned int b;
-	
+	int len;
+
+	len = strspn(s, accept);
+	return (len);
 }
 
