11f84e533cb560cd1bf2fe1869beed3b69f0ea46
bereket-kume <bereketkume@gmail.com>
2023-09-25 04:12:49 -0700

    done

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,9 @@
 #include "main.h"
-/**
+/*
+ *@_memset - that fills memory with a constant byte.
+ *@s: is our first parameter
+ *@n: is our second parameter
+ *return: always s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <string.h>
+/**
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *ret;
+	ret = strstr(haystack, needle);
+	return (ret);
+}
