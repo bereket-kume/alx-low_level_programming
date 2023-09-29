d0ce9717c519e61d41c7cfd7e0b1874808755787
bereket-kume <bereketkume@gmail.com>
2023-09-29 12:44:02 -0700

    done

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -3,24 +3,24 @@
 #include <stdio.h>
 int is_palindrome(char *s)
 {
-	int i, j;
 	int num = strlen(s);
+	int i = 0;
+	int j = num -1;
 
-	for (i = 0; i < num; i++)
-	{
-		if(s[i] >= 'A' && s[i] <= 'z')
-		{
-			s[i] += 32;
-		}
-	}
-
-	for(i = 0, j = num -1; i < j; i++, j--)
+	while( i < j)
 	{
 		if(s[i] != s[j])
 		{
 			return (0);
 		}
+		else
+		{
+			return (1);
+		}
+		i++;
+		j++;
 	}
 	return (1);
+
 }
 
