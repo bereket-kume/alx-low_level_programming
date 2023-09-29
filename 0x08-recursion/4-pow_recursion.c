8b9ee819d3edebba72f7b42fefd365bbffcbfa23
bereket-kume <bereketkume@gmail.com>
2023-09-29 05:24:00 -0700

    done

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,7 +3,7 @@
  *_pow_recursion - is function returns the value of x raised to the power of y
  *@x: is our first parameter
  *@y: is our second parameter
- *Retrun: always
+ *Return: always
  */
 int _pow_recursion(int x, int y)
 {
