9527750a5d432a1a8573ce8959c125b49cc3b744
bereket-kume <bereketkume@gmail.com>
2023-09-29 05:53:42 -0700

    done

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,9 +1,14 @@
 #include "main.h"
+/**
+ *is_prime_number - is function returns
+ *@n: is our first parameter
+ *Return: 1 or 0
+ */
 int is_prime_number(int n)
 {
 	int i;
 
-	if ( n <= 1)
+	if (n <= 1)
 	{
 		return (0);
 	}
@@ -11,6 +16,8 @@ int is_prime_number(int n)
 	{
 		if (i % 2 == 0)
 			return (0);
+		else
+			return (1);
 	}
 	return (1);
 }
