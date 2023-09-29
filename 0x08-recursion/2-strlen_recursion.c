07280c39bb528b5bcb9e8294bded9c0ff5136373
bereket-kume <bereketkume@gmail.com>
2023-09-29 04:49:48 -0700

    done

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,13 +1,18 @@
 #include "main.h"
+/**
+ *_strlen_recursion - s function hat return length of string
+ *@s: is our first parameter
+ *Return: 0 and length of string
+ */
 
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return 0;
+		return (0);
 	}
 	else
 	{
-		return 1 + _strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 	}
 }
