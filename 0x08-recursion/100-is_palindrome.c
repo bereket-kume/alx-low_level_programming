d095f4af0fe632d2370337374651ebea5e25251d
bereket-kume <bereketkume@gmail.com>
2023-09-29 12:53:20 -0700

    check

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,26 +1,26 @@
 #include "main.h"
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
+/**
+ *is_palindrome - is function that display string
+ *@s: is our first parameter
+ *Return: 1 or 0
+ */
 int is_palindrome(char *s)
 {
 	int num = strlen(s);
 	int i = 0;
-	int j = num -1;
+	int j = num - 1;
 
-	while( i < j)
+	while (i < j)
 	{
-		if(s[i] != s[j])
+		if (s[i] != s[j])
 		{
 			return (0);
 		}
-		else
-		{
-			return (1);
-		}
 		i++;
-		j++;
+		j--;
 	}
 	return (1);
-
 }
-
