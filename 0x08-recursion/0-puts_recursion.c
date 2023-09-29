5c17cf20a4cd28110e44c6e232b9b15d901d9a32
bereket-kume <bereketkume@gmail.com>
2023-09-29 04:19:42 -0700

    done

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,7 +6,7 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\n')
+	if (*s == '\0')
 	{
 		return;
 	}
@@ -15,5 +15,6 @@ void _puts_recursion(char *s)
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
+	_putchar('\n');
 
 }
