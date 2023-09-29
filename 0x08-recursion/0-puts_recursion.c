683531697ad233e034a8ba7d566b9d6696e8a5db
bereket-kume <bereketkume@gmail.com>
2023-09-29 03:55:50 -0700

    done

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *_puts_recursion - is function that print string and new
+ *@s: is our firtst parameter
+ *Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(s);
+	_putchar('\n');
+}
