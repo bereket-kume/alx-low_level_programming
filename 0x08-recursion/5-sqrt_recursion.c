66f39f2d49aa974fb962a26c241718a9f12a3b0b
bereket-kume <bereketkume@gmail.com>
2023-09-30 11:58:54 -0700

    done

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,9 +1,14 @@
 #include "main.h"
-
-int _sqrt_recursion(int n) {
-	     if (n == 1 || n == 0)
-	     {
-		     return n;
-	     }
-	     return (1);
+/**
+ *_sqrt_recursion - is function that give square
+ *@n: is our first parameter
+ *Return: alwayas n
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 1 || n == 0)
+	{
+		return (n);
+	}
+	return (1);
 }
