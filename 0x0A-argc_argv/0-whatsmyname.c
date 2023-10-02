661b51c1a69b396934ecd1203a995e547f44ccbe
bereket-kume <135522204+bereket-kume@users.noreply.github.com>
2023-10-02 07:32:10 -0700

    done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function that print member name
+ * @argc: our first paramete
+ * @argv: our second parameter
+ *
+ * Return: Always 0 
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
