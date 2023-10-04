8e96e96f1807e2661822dd9a8265c55e84db0120
bereket-kume <bereketkume@gmail.com>
2023-10-04 13:56:43 -0700

    done

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+char* str_concat(char* s1, char* s2) {
+	    if (s1 == NULL)
+		            s1 = "";
+	        if (s2 == NULL)
+			        s2 = "";
+
+		    int len1;
+		   len1 = strlen(s1);
+		   int len2;
+		  len2 = strlen(s2);
+
+			    char* result = (char*)malloc((len1 + len2 + 1) * sizeof(char));
+
+			        if (result == NULL)
+					        return NULL;
+
+				    strcpy(result, s1);
+				        strcat(result, s2);
+
+					    return result;
+}
