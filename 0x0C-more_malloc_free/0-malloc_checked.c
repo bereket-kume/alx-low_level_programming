99f7edcae42b348b01704edba1b8d371a85c3e3f
bereket-kume <bereketkume@gmail.com>
2023-10-06 05:23:28 -0700

    done

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,16 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-
+/**
+ *malloc_checked - is function that returns
+ *@b: is our first parameter
+ *Return: always ptr
+ */
 void *malloc_checked(unsigned int b)
 {
 	void *ptr = malloc(b);
-	if(ptr == NULL)
+
+	if (ptr == NULL)
 	{
 		exit(98);
 	}
