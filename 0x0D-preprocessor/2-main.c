88d887f90b2eda9961c3d5fb12d40036abde3677
bereket-kume <bereketkume@gmail.com>
2023-10-09 12:41:26 -0700

    done

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
-int main(int argc,char **argv)
+/**
+ *main - is function that print file
+ *@argc: si our first function
+ *@argv: is our second parameter
+ *Return: always 0
+ */
+int main(int argc, char **argv)
 {
-	if(argc > 1)
+	if (argc > 1)
 	{
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	}
-	return 0;
+	return (0);
 }
