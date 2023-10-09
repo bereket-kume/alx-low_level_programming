50418880b09a9e60fa5380859a9b7cf2c5a5f27c
bereket-kume <bereketkume@gmail.com>
2023-10-09 13:39:41 -0700

    done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef CONSTANT_H
-#define CONSTANT_H
+#ifndef _HEADER_
+#define _HEADER_
 
 struct dog
 {
@@ -8,5 +8,11 @@ struct dog
 	char *owner:
 
 };
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
