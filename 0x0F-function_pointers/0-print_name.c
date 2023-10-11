63c16f523f8017547a66f684ee758a747ec296d7
bereket-kume <bereketkume@gmail.com>
2023-10-11 04:22:14 -0700

    done

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,10 @@
-#include <"function_pointers.h">
+#include "function_pointers.h"
 #include <stdio.h>
+/**
+ *print_name - is function that prints a name
+ *@name: is our first function
+ *@f: is our second parameter
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
