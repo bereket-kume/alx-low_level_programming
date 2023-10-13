8f0678843d8db31a716e666ee0f8bb9917660a73
bereket-kume <bereketkume@gmail.com>
2023-10-13 13:43:38 -0700

    done

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,3 +1,7 @@
+#ifndef _VARDAIC_FUNCTION_
+#define _VARDAIC_FUNCTION_
+
 int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+#endif
