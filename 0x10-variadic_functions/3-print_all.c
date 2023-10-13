eb3ee3c70e678acc075c25b4a4d499c6cede0d4f
bereket-kume <bereketkume@gmail.com>
2023-10-13 14:26:31 -0700

    sone

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,22 +1,24 @@
 #include <stdarg.h>
-#include "variadic_functions"
-
+#include "variadic_functions.h"
+#include <stdio.h>
 void print_all(const char* const format, ...)
 {
     va_list args;
     va_start(args, format);
 
-    int i = 0;
+    int i;
     char c;
     float f;
     char* s;
-
+    int num;
+	
+    i=0;
     while (format[i] != '\0') {
         if (format[i] == 'c') {
             c = va_arg(args, int);
             printf("%c", c);
         } else if (format[i] == 'i') {
-            int num = va_arg(args, int);
+            num = va_arg(args, int);
             printf("%d", num);
         } else if (format[i] == 'f') {
             f = va_arg(args, double);
