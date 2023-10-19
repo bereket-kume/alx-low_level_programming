d41476c4cb4783bdd8f512f3c97cbd0af991eeda
bereket-kume <bereketkume@gmail.com>
2023-10-19 12:31:29 -0700

    done

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,16 +1,27 @@
 #include "lists.h"
-
+/**
+ *print_list - is function that print element
+ *@h: is argument of the function
+ *Return: always length
+ */
 size_t print_list(const list_t *h)
 {
-	int length = 0;
-	list_t* amma = h;
-	while (amma != NULL)
+	size_t length = 0;
+
+	while (h)
 	{
-		_putchar(amma->str);
-		length++;
-		amma = amma->next;
+		if (h == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("%d%s\n", h->len, h->str);
+			length++;
+			h = h->next;
+		}
 	}
-	putchar('\n');
-	return (0);
+
+	return (length);
 
 }
