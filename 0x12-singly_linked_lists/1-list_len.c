5fc7b4ad1caf52533ed3827b8ca69c7630a83700
bereket-kume <bereketkume@gmail.com>
2023-10-19 12:48:03 -0700

    done

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,8 +1,14 @@
 #include "lists.h"
+/**
+ *list_len - is function that return lenght
+ *@h: is argument
+ *Return: len
+ */
 size_t list_len(const list_t *h)
 {
 	size_t len = 0;
-	while(h != NULL)
+
+	while (h != NULL)
 	{
 		len++;
 		h = h->next;
