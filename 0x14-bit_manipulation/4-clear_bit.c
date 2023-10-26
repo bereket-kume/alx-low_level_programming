d89fea9c2824a9670ee1efcf64e99b555b374b82
bereket-kume <bereketkume@gmail.com>
2023-10-26 12:25:22 -0700

    done

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *clear_bit - is function that clear bit
+ *@n: is our first parameter
+ *@index: is our second parmater
+ *Return: 1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask = ~(1ul << index);
+
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	*n & mask;
+	return (1);
+}
