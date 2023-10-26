4fd72f7d037d3b2728a847cd3363848833b86442
bereket-kume <bereketkume@gmail.com>
2023-10-26 11:46:37 -0700

    done

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include "main.h"
+vid print_binary(unsigned long int n)
+{
+	int numbits = sizeof(numbits)*8;
+	int i;
+
+	if (n == 0)
+	{
+		_putchar('0);
+		return;
+	}
+	for(i = numbits -1;i >= 0; i--)
+	{
+		unsigned long int mask = 1u << 1;
+		_putchar((n & mask)? '1' : '0');
+	}
+}
