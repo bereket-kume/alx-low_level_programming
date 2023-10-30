b4195e88bf4a67037f1067f150c3f55ce95bf8db
bereket-kume <bereketkume@gmail.com>
2023-10-30 14:26:11 -0700

    done

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,9 @@
 #include "main.h"
 /**
  *read_textfile - is function thaat read from file
+ *@filename; is parmaeter
+ *@letters; parameter
+ *return: everything
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -10,24 +13,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	text = malloc(letters);
 	if (text == NULL)
 		return (0);
-
 	if (filename == NULL)
 		return (0);
-
 	file = open(filename, O_RDONLY);
-
 	if (file == -1)
 	{
 		free(text);
 		return (0);
 	}
-
 	let = read(file, text, letters);
 
 	w = write(STDOUT_FILENO, text, let);
-
 	close(file);
-
 	return (w);
 }
 
