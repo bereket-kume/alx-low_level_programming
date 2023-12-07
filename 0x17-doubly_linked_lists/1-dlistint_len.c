0e069e1bb97788ab701440f715852edc516ede4f
bereket-kume <bereketkume@gmail.com>
2023-12-07 12:36:46 -0800

    done

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *dlistint_len - is function that return lenght
+ *@h: is head of our list
+ *Return: always x
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const distint_t *curretNode = h;
+	size_t x = 0
+
+	while (currentNode != NULL)
+	{
+		x++;
+	}
+	return (x);
+}
