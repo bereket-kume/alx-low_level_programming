2ad537c5e58fef42bdcc650fbff0ded5d64f9a9c
bereket-kume <135522204+bereket-kume@users.noreply.github.com>
2023-12-07 12:05:23 -0800

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,14 +1,16 @@
 #include "lists.h"
+#include <stdio.h>
+
 size_t print_dlistint(const dlistint_t *h)
 {
-	const dlistint_s *currentNode = h;
-	size_t x = 0;
+    const dlistint_t *currentNode = h;
+    size_t x = 0;
 
-	while (currentNode != NULL)
-	{
-		printf("%d", currentNode->n);
-		currentNode = currentNode->next;
-		x++;
-	}	
-	return x;
+    while (currentNode != NULL)
+    {
+        printf("%d", currentNode->n);
+        currentNode = currentNode->next;
+        x++;
+    }
+    return x;
 }
